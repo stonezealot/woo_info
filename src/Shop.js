a65df3cb39df8085648c703d60b8075fdd91c36e
stonezealot <35825821+stonezealot@users.noreply.github.com>
2020-01-15 16:32:59 +0800

    location

--- src/Shop.js
+++ src/Shop.js
@@ -29,13 +29,35 @@ class Shop extends Component {
         }
 
         this.onChange = this.onChange.bind(this)
-        this.getLocation = this.getLocation.bind(this)
 
     }
 
     componentDidMount() {
         document.title = '门店列表'
-        this.getLocation()
+        const BMap = window.BMap;
+        const BMAP_STATUS_SUCCESS = window.BMAP_STATUS_SUCCESS;
+
+        var map = new BMap.Map("allmap");
+        var point = new BMap.Point(116.331398, 39.897445);
+        map.centerAndZoom(point, 12);
+
+        var geolocation = new BMap.Geolocation();
+        var that = this;
+        geolocation.getCurrentPosition(function (r) {
+            if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+                var mk = new BMap.Marker(r.point);
+                map.addOverlay(mk);
+                map.panTo(r.point);
+                console.log('您的位置：' + r.point.lng + ',' + r.point.lat);
+                that.setState({
+                    lng: r.point.lng,
+                    lat: r.point.lat
+                })
+            }
+            else {
+                console.log('failed' + this.getStatus());
+            }
+        }, { enableHighAccuracy: true })
 
 
     }
@@ -46,36 +68,6 @@ class Shop extends Component {
         }, () => { console.log(this.state.value) })
 
     };
-
-    getLocation() {
-        const BMap = window.BMap;
-        const BMAP_STATUS_SUCCESS = window.BMAP_STATUS_SUCCESS;
-
-        var map = new BMap.Map("allmap");
-        var point = new BMap.Point(116.331398, 39.897445);
-        map.centerAndZoom(point, 12);
-
-        var geolocation = new BMap.Geolocation();
-        geolocation.getCurrentPosition(function (r) {
-            if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-                var mk = new BMap.Marker(r.point);
-                map.addOverlay(mk);
-                map.panTo(r.point);
-                console.log('您的位置：' + r.point.lng + ',' + r.point.lat);
-                this.setState({
-                    lng: r.point.lng,
-                    lat: r.point.lat
-                })
-            }
-            else {
-                console.log('failed' + this.getStatus());
-            }
-        }, { enableHighAccuracy: true })
-    }
-
-
-
-
 
     render() {
 
