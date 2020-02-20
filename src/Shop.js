049e01ad6e2f7bea04015a6c8ee330f12abf1b65
stonezealot <35825821+stonezealot@users.noreply.github.com>
2020-02-21 00:57:24 +0800

    shoplist

--- src/Shop.js
+++ src/Shop.js
@@ -185,7 +185,7 @@ class Shop extends Component {
                     alignItems: 'center',
                 }}>
                     {
-                        obj== undefined ?
+                        obj == undefined ?
 
                             null :
 
@@ -232,7 +232,7 @@ class Shop extends Component {
                             pageSize={5}
                         />
                     </div>
-                    {/* <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#F7F7F7' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#F7F7F7' }}>
                         <ListView
                             key={this.state.useBodyScroll ? '0' : '1'}
                             ref={el => this.lv = el}
@@ -249,7 +249,7 @@ class Shop extends Component {
                             onEndReached={this.onEndReached}
                             pageSize={5}
                         />
-                    </div> */}
+                    </div>
                 </Tabs>
             </div>
         );
